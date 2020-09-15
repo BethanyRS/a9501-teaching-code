627ac89b6b6c8be59f16433febd09ef1ce18c8b0
Pauline Barmby <pbarmby@uwo.ca>
2020-09-14 20:28:13 -0400

    Update assert_demo.py

--- assert_demo.py
+++ assert_demo.py
@@ -21,3 +21,36 @@ def normalize_rectangle(rect):
     assert 0 < upper_y <= 1.0, 'Calculated upper Y coordinate invalid'
 
     return (0, 0, upper_x, upper_y)
+
+
+# Suppose we need to find where two or more time series overlap. 
+# The range of each time series is represented as a pair of numbers, which are the time the interval started and ended. 
+
+def test_range_overlap():
+    assert range_overlap([ (0.0, 1.0), (5.0, 6.0) ]) == None
+    assert range_overlap([ (0.0, 1.0), (1.0, 2.0) ]) == None
+    assert range_overlap([ (0.0, 1.0) ]) == (0.0, 1.0)
+    assert range_overlap([ (2.0, 3.0), (2.0, 4.0) ]) == (2.0, 3.0)
+    assert range_overlap([ (0.0, 1.0), (0.0, 2.0), (-1.0, 1.0) ]) == (0.0, 1.0)
+    assert range_overlap([]) == None
+
+#Take a moment to think about why we calculate the left endpoint of the overlap as the maximum of the input left endpoints, 
+# and the overlap right endpoint as the minimum of the input right endpoints. 
+def range_overlap(ranges):
+    """Return common overlap among a set of [left, right] ranges."""
+    max_left = 0.0
+    min_right = 1.0
+    for (left, right) in ranges:
+        max_left = max(max_left, left)
+        min_right = min(min_right, right)
+    return (max_left, min_right)
+
+def get_total(values):
+    assert len(values) > 0
+    for element in values:
+        assert int(element)
+    values = [int(element) for element in values]
+    total = sum(values)
+    assert total > 0
+    return total
+
